dbac0ba97d98f8954b4bab0555879cad5902737a
bobiscool <tomisugly@163.com>
2017-07-20 17:45:15 +0800

    [Chg] 打包

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let willClose = false;
 
 function createWindow () {
     win = new BrowserWindow({
-        width: 380,
+        width: 420,
         // width: 1000,
         height: 650,
         title: 'iBiu',
